7d95e59c1cc8c699761e03e41cd329ed536b20e1
Ravi Singh <cr.cr420@gmail.com>
2023-05-23 17:40:48 +0530

    test.cs file added

---
+++ test.cs
@@ -0,0 +1,13 @@
+using System;
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int x = 5;
+      int y = 6;
+      Console.WriteLine("product of x and y is"+(x * y));
+    }
+  }
+}
